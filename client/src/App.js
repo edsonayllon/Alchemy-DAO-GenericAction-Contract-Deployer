ec3c69b4923d36e2a47aae0b7bbfe39569806144
eayllon1 <eayllon1@gmail.com>
2019-09-29 19:02:29 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,10 @@ const optionByNetwork = {
   rinkeby: {
     votingMachineAddress: '0x7648665cda324511b71e002E9C62da98a8E68505', // Genesis Protocol
     targetContractAddress: '0x38f1886081759f7d352c28984908d04e8d2205a6', // standard bounties contract
-    daoAddress: '0x72939947e7a1c4ac94bb840e3304b322237ad1a8'
-  }, 
+    daoAddress: '0x72939947e7a1c4ac94bb840e3304b322237ad1a8' // Genesis Alpha 
+  },
   main: {
-    votingMachineAddress: '0x8DDCEF56944094DF8ef8836A6f8168a75a133192', // Genesis Protocol 
+    votingMachineAddress: '0x8DDCEF56944094DF8ef8836A6f8168a75a133192', // Genesis Protocol
     targetContractAddress: '0x43ee232734097B07803Ea605b49C6eE6Bf10f8cc', // standard bounties contract
     daoAddress: '0x294f999356ed03347c7a23bcbcf8d33fa41dc830' // Genesis Alpha DAO
   }
@@ -47,56 +47,48 @@ function App() {
 
     // Log Address of new instance to use in next step while registering the scheme to DAO
     console.log(`Deployed new GenericScheme instance at ${genericScheme.options.address}`)
-    
+
     // Following are example values, Please change appropriately
-    // Refer https://daostack.zendesk.com/hc/en-us/sections/360000535638-Genesis-Protocol
+    // To get, see https://daotalk.org/t/how-to-use-the-scheme-registrar-in-alchemy/669
+    // or https://github.com/edsonayllon/Alchemy-DAO-Scheme-Param-Hash-Generator
     // current parameters based on https://daotalk.org/t/the-contentious-genesis-parameter-delta/481
-    const voteParams = {
-      "boostedVotePeriodLimit": 345600, // voting period, boosted votes are fast tracked
-      "daoBountyConst": 10, // determines automatic downstaking value by multiplying by average
-      "minimumDaoBountyGWei": 150000000000, // minimum amount of GEN a DAO will stake when automatically downstaking each proposal
-      "queuedVotePeriodLimit": 2592000, // voting time for nonboosted proposals
-      "queuedVoteRequiredPercentage": 50, // quorum to decide votes (nonboosted)
-      "preBoostedVotePeriodLimit": 86400, // time proposal must be above boosted threshold before being boosted
-      "proposingRepRewardGwei": 50000000000, // controls how much voting power someone who submits a proposal gains
-      "quietEndingPeriod": 172800, // time votes need to stay the same to be considered the final result
-      "thresholdConst": 1200, // controls rate of required confidence score for boosting rising as currently boosted proposals rises.
-      "voteOnBehalf": "0x0000000000000000000000000000000000000000", // https://daotalk.org/t/how-to-use-the-scheme-registrar-in-alchemy/669 kept this as null address
-      "votersReputationLossRatio": 4, // percentage lost if vote is against the final outcome for nonboosted
-      "activationTime": 0 // time when proposing and voting is activated (unix)
-    }
+    const voteParams = "0xde924589f841ec9b5ea74a57f0063166c5330e7d40152ebb6d9b8c9f42a89254"
 
     // Get address from https://github.com/daostack/migration/blob/master/migration.json
     const votingMachineAddress = optionByNetwork[network].votingMachineAddress; // address of voting machine for a network, defaults to Genesis Protocol
 
     // For eg if you want this Generic Scheme to enable DAO to interact with Bounties Network
     // then targetContract would be the address of Bounties Network's respective contract
-    const targetContractAddress = optionByNetwork[network].targetContractAddress; // contract generic scheme queries 
-    
+    const targetContractAddress = optionByNetwork[network].targetContractAddress; // contract generic scheme queries
+
     const avatar = optionByNetwork[network].daoAddress; // address of DAO
 
     // paramHash will be useful in later step so lets log it
-    // Mimic getParametersHash(): https://github.com/daostack/infra/blob/master/contracts/votingMachines/GenesisProtocolLogic.sol 
+    // Mimic getParametersHash(): https://github.com/daostack/infra/blob/master/contracts/votingMachines/GenesisProtocolLogic.sol
     // This method shown here won't work as encodePacked is used: https://ethereum.stackexchange.com/questions/60144/creating-hash-of-a-struct-in-solidity-and-javascript
     // Simpler would be to use the contract deployed, instructions here: https://daotalk.org/t/how-to-use-the-scheme-registrar-in-alchemy/669
     // alternatvely, we can create a copy of that smart contract function, and call it locally
-    const paramHash = genericScheme.methods.initialize(
+    const paramHash = await genericScheme.methods.initialize(
       avatar,
       votingMachineAddress,
       voteParams,
       targetContractAddress
-    ).call()
+    ).call((err, result) => {
+      console.log(err);
+      console.log(result);
+    })
 
-    console.log(paramHash)
+    //console.log(paramHash)
 
-    // calls initialize method in generic scheme contract 
+    // calls initialize method in generic scheme contract
     /// https://github.com/daostack/arc/blob/master/contracts/schemes/GenericScheme.sol
-    genericScheme.methods.initialize(
+    const result = await genericScheme.methods.initialize(
       avatar,
       votingMachineAddress,
       voteParams,
       targetContractAddress
-    ).send()
+    ).send();
+    // console.log(result)
   }
 
   useEffect(()=>{
